5f2dd8e83e9ab50331160364b94c11b07d421d26
Mario <53944334+mario-im@users.noreply.github.com>
2024-06-27 18:47:59 +0200

    test

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-// service-worker.js
+// worker.js
 self.addEventListener('install', (event) => {
     console.log('Service worker installing...');
     // Aggiungi assets alla cache qui
@@ -13,19 +13,3 @@ self.addEventListener('install', (event) => {
     console.log('Fetching:', event.request.url);
     // Intercetta le richieste e risponde con le cache se necessario
   });
-
-  // worker.js
-self.addEventListener('install', (event) => {
-  console.log('Service worker installing...');
-  // Aggiungi assets alla cache qui
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activating...');
-  // Pulisci vecchie cache se necessario
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
-  // Intercetta le richieste e risponde con le cache se necessario
-});
