a5c95e2dc14c3fcb5100de2d46ef6bbadae13f06
Mario <53944334+mario-im@users.noreply.github.com>
2024-06-27 12:33:06 +0200

    faq

---
+++ FAQ/faq.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const faqContainer = document.getElementById('faq-container');
+  
+    fetch('https://raw.githubusercontent.com/mario-im/cetriolo/main/FAQ/faq.json')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach((faq, index) => {
+          const faqItem = document.createElement('div');
+          faqItem.classList.add('accordion-item');
+  
+          const button = document.createElement('button');
+          button.id = `accordion-button-${index + 1}`;
+          button.setAttribute('aria-expanded', 'false');
+          button.innerHTML = `<span class="accordion-title">${faq.title}</span><span class="icon" aria-hidden="true"></span>`;
+  
+          const content = document.createElement('div');
+          content.classList.add('accordion-content');
+          content.innerHTML = `<p>${faq.content}</p>`;
+  
+          faqItem.appendChild(button);
+          faqItem.appendChild(content);
+          faqContainer.appendChild(faqItem);
+        });
+  
+        const items = document.querySelectorAll(".accordion button");
+  
+        function toggleAccordion() {
+          const itemToggle = this.getAttribute('aria-expanded');
+  
+          for (let i = 0; i < items.length; i++) {
+            items[i].setAttribute('aria-expanded', 'false');
+          }
+  
+          if (itemToggle == 'false') {
+            this.setAttribute('aria-expanded', 'true');
+          }
+        }
+  
+        items.forEach(item => item.addEventListener('click', toggleAccordion));
+      })
+      .catch(error => {
+        console.error('Errore nel caricamento delle FAQ:', error);
+      });
+  });
